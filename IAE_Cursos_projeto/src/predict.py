ebbfa4dfa5d0b8b6a656e6bac3799669992e77b9
Gabriel Soares <74696315+Gsdvl@users.noreply.github.com>
2023-06-11 07:55:53 -0300

    Alteração no predict.py
    Agora a saída é armazenada em um arquivo de texto ao invés de apenas imprimir no console

--- IAE_Cursos_projeto/src/predict.py
+++ IAE_Cursos_projeto/src/predict.py
@@ -1,4 +1,7 @@
+import sys
 from train import *
+
+
 
 #Carrega o modelo
 with open('../bin/model.pkl', 'rb') as mdl:
@@ -12,8 +15,24 @@ with open('../content/preferencias.txt', 'r') as file:
 input_test = [[], [], [], []]
 for i, v in enumerate(preferencias.split()):
     input_test[i].append(int(v))
-    
+
+
+# Abre o arquivo em modo de escrita, truncando-o para ficar vazio
+with open('../content/saida_predict.txt', 'w') as file:
+    file.truncate(0)
+
+# Redireciona a saída padrão para o arquivo
+sys.stdout = open('../content/saida_predict.txt', 'w')
+
+
 #Resultado do modelo com os novos inputs
 predict = loaded_model.predict(input_test)
 for i in predict:
     print(int(i))
+
+
+# Fecha o arquivo
+sys.stdout.close()
+
+# Restaura a saída padrão para o console
+sys.stdout = sys.__stdout__
