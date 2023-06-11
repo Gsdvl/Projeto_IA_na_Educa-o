e99691f796848ffd7b5e74b22db89685737d6c82
Gabriel Soares <74696315+Gsdvl@users.noreply.github.com>
2023-06-11 07:40:34 -0300

    Alteração da main.py
    Agora não é mais necessário executar os arquivos: train.py e predict.py individualmente, a main já os executa

--- IAE_Cursos_projeto/src/main.py
+++ IAE_Cursos_projeto/src/main.py
@@ -1,5 +1,20 @@
 import subprocess
 
+def executarModeloDeepLearning():
+    caminho_arquivo_train = 'train.py'
+    caminho_arquivo_predict = 'predict.py'
+    
+    #Roda o arquivo de treino
+    processo_treino = subprocess.Popen(['python',caminho_arquivo_train])
+    
+    #Espera ele terminar de ser executado
+    processo_treino.wait()
+
+    #Roda o modelo de DL
+    processo_predict = subprocess.Popen(['python',caminho_arquivo_predict])
+
+    #Espera o mesmo terminar de ser executado mas podendo interagir com as saidas
+    processo_predict.communicate()
 
 
 def executarCpp():
@@ -33,7 +48,8 @@ executarR()
 print("#### Código em R executado ######")
 
 print("Pronto para Rodar o código em python...")
-
+executarModeloDeepLearning()
+print("#### Modelo de Deep Learning Executado ######")
 # * A base de dados vai ser salva com o nome udemy_courses_filtred.csv
 # TODO: A partir daqui começa a main
 
